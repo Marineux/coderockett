102ac3db505e72d2bc014bb837d71b364e0bbb33
Marineux <firselee@gmail.com>
2021-09-29 08:42:20 +0700

    responsive tablet new image

--- js/apk.js
+++ js/apk.js
@@ -1,12 +1,23 @@
-var myNavbar = document.querySelector('.navbar')
-window.onscroll = function () {
-    "use strict";
-    if (document.body.scrollTop >= 200 ) {
-        mayNavbar.classList.add(".scroll");
-        myNavbar.classList.remove(".scroll");
-    }
-    else {
-        myNavbar.classList.remove("none");
-        myNavbar.classList.add("none");
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+// var myNavbar = document.querySelector('.navbar')
+// window.onscroll = function () {
+//     "use strict";
+//     if (document.body.scrollTop >= 200 ) {
+//         mayNavbar.classList.add(".scroll");
+//         myNavbar.classList.remove(".scroll");
+//     }
+//     else {
+//         myNavbar.classList.remove("none");
+//         myNavbar.classList.add("none");
+//     }
+// }
--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
 const hamburger = document.querySelector('.hamburger input');
 const nav = document.querySelector('nav');
-const ul = document.querySelector('.primary-nav',);
+const ul = document.querySelector('.primary-nav');
 const li = document.querySelector('.secondary-nav');
-hamburger.addEventListener('click', function() {
+hamburger.addEventListener('click', function () {
   nav.classList.toggle('slide');
   ul.classList.toggle('slide');
-  li.classList.toggle('slide')
+  li.classList.toggle('slide');
 });
-
